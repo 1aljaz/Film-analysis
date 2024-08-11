81bc10f7f632e81959b7583ac1dbc27d3f4f9af1
Aljaz Marn <azo.marn@gmail.com>
2024-08-11 20:06:22 +0200

    Prevedel in dodelal bazo
    Prevedel sem v slovenščino. Dodal sem veliko dodatnih podatkov in jih prečistil.

--- main/main.py
+++ main/main.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup as bs
 import pandas as pd
 import requests
 
+num_of_pages = 30  #number of pages it scrapes on the numbers website
+
 headers = {
     'User-Agent': 'Mozilla/5.0 (X11; CrOS x86_64 8172.45.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.64 Safari/537.36',
     'Accept-Language': 'en-US, en;q=0.5'
@@ -19,23 +21,20 @@ class Film:
         self.rank = ""
         self.director = ""
         self.release_date = ""
-        self.genre = ""
         self.dist = ""
 
     def get_data_numbers(self, num=""):
         response = requests.get(url+"/"+num, headers=headers)
         if response.status_code != 200:
-            print(f"Failed to retrieve the page. Status code: {response.status_code}")
+            print(f"Status code: {response.status_code}")
             return
 
         soup = bs(response.text, 'html.parser')
         
         tables = soup.find_all('table')
-        print(f"Found {len(tables)} tables on the page")
 
         for table in tables:
             rows = table.find_all('tr')
-            print(f"Analyzing table with {len(rows)} rows")
             
             for row in rows[1:]: 
                 try:
@@ -51,24 +50,25 @@ class Film:
                         
                         movie_data = {
                             'Rank': self.rank,
-                            'Name': self.name,
-                            'Gross Income': self.gross,
-                            'Audience Score': self.aud_score,
-                            'Critics Score': self.crit_score,
-                            'Director' : self.director,
-                            'Distributor' : self.dist,
-                            'Release date': self.release_date
+                            'Naslov': self.name,
+                            'Zasluzek': self.gross,
+                            'Ocena občinstva': self.aud_score,
+                            'Ocena kritikov': self.crit_score,
+                            'Direktor' : self.director,
+                            'Studio' : self.dist,
+                            'Datum izzida': self.release_date
                         }
                         
-                        self.film_data.append(movie_data)
+                        if self.director != 'N/A':
+                            self.film_data.append(movie_data)
                         
-                        print(f"Processed: Rank: {self.rank}, Name: {self.name}, Gross: {self.gross}, Audience: {self.aud_score}, Critics: {self.crit_score}, Dist: {self.dist}, Direc: {self.director}, Date: {self.release_date}")
+                        print(f"Rank: {self.rank}, ime: {self.name}, Zasl.: {self.gross}, Občinstvo: {self.aud_score}, Kritiki: {self.crit_score}, Studio: {self.dist}, Direktor: {self.director}, Datum: {self.release_date}")
                     else:
                         print(f"Row doesn't have enough columns: {columns}")
                 except Exception as e:
-                    print(f"Error processing row: {e}")
+                    print(e)
         
-        print(f"Total movies processed: {len(self.film_data)}")
+        print(f"Št. filmov: {len(self.film_data)}")
 
     def correct_name(self, name:str):
         s = name.replace(":", "")
@@ -79,7 +79,7 @@ class Film:
         url = f"https://www.rottentomatoes.com/m/{corrected_name}"
         response = requests.get(url, headers=headers)
         if response.status_code != 200:
-            print(f"Failed to retrieve Rotten Tomatoes page for {name}. Status code: {response.status_code}")
+            print(response.status_code)
             self.aud_score = 'N/A'
             self.crit_score = 'N/A'
             self.director = 'N/A'
@@ -88,26 +88,28 @@ class Film:
             return
         
         soup = bs(response.text, 'html.parser')
-        info_dl = soup.find('dl')
+        dl = soup.find('dl')
 
-        if info_dl:
-            for div in info_dl.find_all('div', class_='category-wrap'):
+        if dl:
+            for div in dl.find_all('div', class_='category-wrap'):
                 key = div.find('dt', class_='key').text.strip()
-                value = div.find('dd')
+                beseda = div.find('dd')
         
-                if key == "Director" and value != "":
-                    self.director = value.text.replace("\r", "").strip()
+                if key == "Director" and beseda != "":
+                    self.director = beseda.text.replace("\r", "").strip()
                     self.director = self.director.replace("\n", "").strip()
-                elif key == "Release Date (Theaters)" and value != "":
-                    self.release_date = value.text.strip().replace("Wide", "")
-                elif key == "Distributor" and value != "":
-                    self.dist = value.text.strip()
+                elif key == "Release Date (Theaters)" and beseda != "":
+                    self.release_date = beseda.text.strip().replace("Wide", "")
+                    self.release_date = self.release_date.strip().replace("Original", "")
+                    self.release_date = self.release_date[:-1]
+                elif key == "Distributor" and beseda != "":
+                    self.dist = beseda.text.strip()
                     self.dist = ', '.join([d.strip() for d in self.dist.split('\n') if d.strip()])
 
         def extract_score(slot_name):
-            button = soup.find('rt-button', attrs={'slot': slot_name})
-            if button:
-                rt_text = button.find('rt-text')
+            rt = soup.find('rt-button', attrs={'slot': slot_name})
+            if rt:
+                rt_text = rt.find('rt-text')
                 if rt_text:
                     return str(rt_text.text.strip())
                     
@@ -118,16 +120,15 @@ class Film:
 
 
 
-    def save_to_csv(self, filename='movie_data.csv'):
+    def save_to_csv(self):
         if not self.film_data:
             print("No data to save")
             return
         
         df = pd.DataFrame(self.film_data)
-        df.to_csv(filename, index=False)
-        print(f"Data saved to {filename}")
+        df.to_csv('movie_data.csv', index=False)
 
 f = Film()
-for i in range(0, 505, 101):
+for i in range(0, 101*num_of_pages, 101):
     f.get_data_numbers(str(i))
 f.save_to_csv()
